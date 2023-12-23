c7c07b256f2fc8f78f2ad690cf3d05e6d3623c86
Aziz Limonu <azizlimonu@gmail.com>
2023-12-23 15:50:26 +0800

    setup redux and providers

---
+++ src/redux/tags/types.ts
@@ -0,0 +1,19 @@
+export enum tagTypes {
+  user = "user",
+  services = "services",
+  cart = "cart",
+  booking = "booking",
+  review = "review",
+  feedback = "feedback",
+  faq = "faq",
+}
+
+export const tagTypesList = [
+  tagTypes.user,
+  tagTypes.services,
+  tagTypes.cart,
+  tagTypes.booking,
+  tagTypes.review,
+  tagTypes.feedback,
+  tagTypes.faq,
+];
